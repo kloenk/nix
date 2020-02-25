0888bbda42388d6daf5f1496d2ede5984f712647
Finn Behrens <me@kloenk.de>
2020-02-25 23:16:00 +0100

    disable own pkgs for hydra

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,19 +7,19 @@ let
 
   newpkgs = {
     collectd-wireguard = callPackage ./collectd-wireguard { };
-    engelsystem = callPackage ./engelsystem { };
+    #engelsystem = callPackage ./engelsystem { };
     #rifo = callPackage ./rifo { };
-    rwm = callPackage ./rwm { };
-    dwm = callPackage ./dwm { rwm = newpkgs.rwm; };
-    slstatus = callPackage ./slstatus { };
-    ftb = callPackage ./ftb { libXxf86vm = pkgs.xorg.libXxf86vm; };
+    #rwm = callPackage ./rwm { };
+    #dwm = callPackage ./dwm { rwm = newpkgs.rwm; };
+    #slstatus = callPackage ./slstatus { };
+    #ftb = callPackage ./ftb { libXxf86vm = pkgs.xorg.libXxf86vm; };
     #flameshot = pkgs.libsForQt5.callPackage ./flameshot { };
     #llgCompanion = callPackage ./llgCompanion { };
-    shelfie = callPackage ./shelfie { };
-    pytradfri = callPackage ./pytradfri { buildPythonPackage = pkgs.python37Packages.buildPythonPackage; fetchPypi = pkgs.python37Packages.fetchPypi; cython = pkgs.python37Packages.cython; };
-    aiocoap = callPackage ./aiocoap { buildPythonPackage = pkgs.python37Packages.buildPythonPackage; fetchPypi = pkgs.python37Packages.fetchPypi; cython = pkgs.python37Packages.cython; };
-    inherit (callPackage ./minecraft-server { })
-      minecraft-server_1_14_2;
+    #shelfie = callPackage ./shelfie { };
+    #pytradfri = callPackage ./pytradfri { buildPythonPackage = pkgs.python37Packages.buildPythonPackage; fetchPypi = pkgs.python37Packages.fetchPypi; cython = pkgs.python37Packages.cython; };
+    #aiocoap = callPackage ./aiocoap { buildPythonPackage = pkgs.python37Packages.buildPythonPackage; fetchPypi = pkgs.python37Packages.fetchPypi; cython = pkgs.python37Packages.cython; };
+    #inherit (callPackage ./minecraft-server { })
+    #  minecraft-server_1_14_2;
   };
 
 in newpkgs
