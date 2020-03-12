7509615e672ac31cecefa3cbd132c2a660344c39
Finn Behrens <me@kloenk.de>
2020-03-12 10:59:41 +0100

    hydra foo

--- default.nix
+++ default.nix
@@ -23,21 +23,3 @@ in {
   inherit sources;
 })
 
-#let
-#	hosts = import ./configuration/hosts;
-#	pkgs = import ./sources/nixpkgs { };
-#in {
-#	deploy = import ./lib/krops.nix;
-#	kexec_tarball = import ./lib/kexec-tarball.nix;
-#	isoImage = import ./lib/iso-image.nix;
-#	# pkgs = import ./configuration/pkgs;
-#
-#	update-sources = pkgs.writeScript "update-sources" ''
-#		#!${pkgs.stdenv.shell}
-#		set -xe
-#		cd ${toString ./.}
-#		git submodule foreach git pull
-#		git add sources
-#		git commit -m "update submodules"
-#	'';
-#}
--- release.nix
+++ release.nix
@@ -1,1 +1,18 @@
-import ./default.nix
+{
+  krops
+, home-manager
+, nixpkgs
+, nixos-mailserver
+, secrets
+, hydra ? true
+, ...
+}:
+
+let 
+  sources = {
+    inherit krops home-manager nixpkgs nixos-mailserver secrets hydra;
+  };
+  home = (import ./lib/manager.nix sources).home;
+in {
+  jobsets.home = home;
+}
