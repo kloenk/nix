9056c830441bc066f81704a427d667ca3557adf9
Finn Behrens <me@kloenk.de>
2020-05-12 09:25:40 +0200

    change restic path

--- configuration/desktop/sway-hm.nix
+++ configuration/desktop/sway-hm.nix
@@ -158,8 +158,8 @@
       };
       extraSessionCommands = ''
         		 # needs qt5.qtwayland in systemPackages
-             #export QT_QPA_PLATFORM=wayland QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-             #export SDL_VIDEODRIVER=wayland MOZ_ENABLE_WAYLAND=1 BEMENU_BACKEND=wayland
+             export QT_QPA_PLATFORM=wayland QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+             export SDL_VIDEODRIVER=wayland MOZ_ENABLE_WAYLAND=1 BEMENU_BACKEND=wayland
              # Fix for some Java AWT applications (e.g. Android Studio),
              # use this if they aren't displayed properly:
              export _JAVA_AWT_WM_NONREPARENTING=1
--- configuration/hosts/hubble/restic.nix
+++ configuration/hosts/hubble/restic.nix
@@ -12,13 +12,13 @@
       "/var/lib/postgresql"
       "/var/backup/postgresql"
     ];
-    repository = "rclone:google:restic-nixos";
+    repository = "rclone:google:hubble";
   };
 
   systemd.services.restic-backups-hubble.path = [ pkgs.rclone ];
   systemd.services.restic-backups-hubble.preStart = lib.mkBefore (''
     mkdir -p /root/.config/rclone
-    ln -s ${
+    ln -sf ${
       config.krops.secrets.files."restic/rclone.conf".path
     } /root/.config/rclone/rclone.conf
   '');
