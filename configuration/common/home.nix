56d54477011ea8fae67eca311c48d4f3d06cb941
Finn Behrens <me@kloenk.de>
2020-06-01 21:00:47 +0200

    update vim configs

--- configuration/common/home.nix
+++ configuration/common/home.nix
@@ -126,6 +126,33 @@ in {
         set shiftwidth=2
         "set cc=80
         set nu
+
+        " Uncomment the following to have Vim jump to the last position when
+        " reopening a file
+        if has("autocmd")
+          au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+        endif
+
+        " Move temporary files to a secure location to protect against CVE-2017-1000382
+        if exists('$XDG_CACHE_HOME')
+          let &g:directory=$XDG_CACHE_HOME
+        else
+          let &g:directory=$HOME . '/.cache'
+        endif
+
+        let &g:undodir=&g:directory . '/vim/undo//'
+        let &g:backupdir=&g:directory . '/vim/backup//'
+        let &g:directory.='/vim/swap//'
+        " Create directories if they doesn't exist
+        if ! isdirectory(expand(&g:directory))
+          silent! call mkdir(expand(&g:directory), 'p', 0700)
+        endif
+        if ! isdirectory(expand(&g:backupdir))
+          silent! call mkdir(expand(&g:backupdir), 'p', 0700)
+        endif
+        if ! isdirectory(expand(&g:undodir))
+          silent! call mkdir(expand(&g:undodir), 'p', 0700)
+        endif
       '';
       plugins = with pkgs; [
         vimPlugins.rust-vim
